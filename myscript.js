623b77e0e3b22a3fab69e51e0bad9506ea821dc3
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 13:10:10 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,382 @@
+var myQuestions = [
+  {
+    question: "1. Some scientists say that dinosaurs died because there was not enough oxygen in the atmosphere. <span style='text-decoration:underline'>They</span> have found tiny bubbles of air in stones called amber."
+    ,
+    answers: {
+      a: "dinosaurs",
+      b: 'scientists',
+      c: 'bubbles',
+      d: 'stones'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "2. The small village was so far from any populated area that <span style='text-decoration:underline'>it</span> did not have any electricity until this year. The government built solar collectors to use the sun’s energy directly.",
+    answers: {
+      a: 'electricity',
+      b: "sun's energy",
+      c: 'food storage',
+      d: 'small village'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "3. His father had warned <span style='text-decoration:underline'>him</span> to stay out of the road. He told the boy many times about the danger of cars on the busy street. Nevertheless, the little boy ran across the road.",
+    answers: {
+      a: 'father',
+      b: 'brother',
+      c: 'boy',
+      d: 'uncle'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "4. There was a story in the newspaper about a new apartment building. The advertisement gave a lot of information about <span style='text-decoration:underline'>it</span>.",
+    answers: {
+      a: 'announcement',
+      b: 'newspaper',
+      c: 'information',
+      d: 'new apartment building'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "5. During the first week of school, the art teacher asked the children to draw a lot of pictures. <span style='text-decoration:underline'>He</span> studied their pictures of themselves, their families, and their homes.",
+    answers: {
+      a: 'art teacher',
+      b: 'child',
+      c: 'father',
+      d: 'student'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "6. People with diabetes are advised by doctors to eat carefully. For example, when buying different foods, they should choose <span style='text-decoration:underline'>those</span> with low sugar content. ",
+    answers: {
+      a: 'doctors',
+      b: 'foods',
+      c: 'sugar',
+      d: 'eating carefully'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "7. Different kinds of knots are used for different purposes. <span style='text-decoration:underline'>One</span> is used for tying up boats and another for tying shoelaces.",
+    answers: {
+      a: 'a type',
+      b: 'a dozen',
+      c: 'a knot',
+      d: 'a purpose'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "8. Taif, a mountainous place <span style='text-decoration:underline'>that</span> has a cool climate, is a popular destination in the kingdom during summer.",
+    answers: {
+      a: 'place',
+      b: 'destination',
+      c: 'climate',
+      d: 'kingdon'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "9. Your chances of avoiding an accident are better in a car in good condition than they are in <span style='text-decoration:underline'>one</span> in bad condition.",
+    answers: {
+      a: 'chance',
+      b: 'accident',
+      c: 'condition',
+      d: 'car'
+
+    },
+    correctAnswer: 'd'
+  },
+  
+  {
+    question: "10. Studies show that drivers who use <span style='text-decoration:underline'>their</span> seatbelts are more likely to drive carefully, obey traffic rules and regulations and avoid a crashes.",
+    answers: {
+      a: 'seatbelts',
+      b: 'drivers',
+      c: 'traffic rules',
+      d: 'regulations'
+
+    },
+    correctAnswer: 'b'
+  },
+  
+  {
+    question: "11. In a news conference this afternoon, the university announced that <span style='text-decoration:underline'>it</span> intends to make some changes in next year’s budget ",
+    answers: {
+      a: 'budget',
+      b: 'unversity',
+      c: 'next year',
+      d: 'conference'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "12. The committee has made the suggestion to all foreign investors that <span style='text-decoration:underline'>they</span> should study all applicable laws before buying property.",
+    answers: {
+      a: 'investors',
+      b: 'laws',
+      c: 'property',
+      d: 'committee'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "13. A normal exploration ship has the equipment <span style='text-decoration:underline'>it</span> needs to record data about deposits on the bottom of the sea.",
+    answers: {
+      a: 'floor',
+      b: 'equipment',
+      c: 'ship',
+      d: 'exploration'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "14. Close to Antarctica are the islands of South Georgia and the Falklands. Both British possessions, <span style='text-decoration:underline'>they</span> are cold and windswept.",
+    answers: {
+      a: 'the British',
+      b: 'possessions',
+      c: 'Antarctica',
+      d: 'South Georgia and Falklands'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "15. The book is about the man <span style='text-decoration:underline'>who</span> invented the telephone.",
+    answers: {
+      a: 'book',
+      b: 'man',
+      c: 'invented',
+      d: 'telephone'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "16. The police are still looking for the body <span style='text-decoration:underline'>that</span> has been missing for a week.",
+    answers: {
+      a: 'the police',
+      b: 'last week',
+      c: 'the body',
+      d: 'the police and the body'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "17. Fuse boxes are installed out of reach of children to protect <span style='text-decoration:underline'>them</span> from bare wires that could electrocute them.",
+    answers: {
+      a: 'children',
+      b: 'seconds',
+      c: 'fuse boxes',
+      d: 'wires'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "18. Parents like to take their families on the same sort of holidays <span style='text-decoration:underline'>they</span> were taken on as children.",
+    answers: {
+      a: 'parents',
+      b: 'children',
+      c: 'holidays',
+      d: 'families'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "19. Doctors are developing drugs <span style='text-decoration:underline'>that</span> will stop rejection and increase the number of successful operations.",
+    answers: {
+      a: "doctors",
+      b: "operations",
+      c: "drugs",
+      d: 'rejection'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "20. There are several major language families in the world. Some scientists say there are nine families, but other scientists divide <span style='text-decoration:underline'>them</span> differently.",
+    answers: {
+      a: "languages",
+      b: "scientists",
+      c: "language families",
+      d: 'scientists and languages'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "21. Parents began to realize that they had to take good care of their children. They had to take care of their health and had to give <span style='text-decoration:underline'>them</span> an education..",
+    answers: {
+      a: "children",
+      b: "parents",
+      c: "education",
+      d: "children and parents"
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "22. Larry was very embarrassed when he could not remember his teacher’s name. It was <span style='text-decoration:underline'>his</span> encouragement that made Larry stay in the course.",
+    answers: {
+      a: "Larry",
+      b: "course",
+      c: "teacher",
+      d: "name"
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "23. At the counter of a modern supermarket, the cashier doesn’t have to manually enter the price of each item on the cash register. There is a special device that is attached to the register and <span style='text-decoration:underline'>it</span> detects accurately the price of goods.",
+    answers: {
+      a: "counter",
+      b: "supermarket",
+      c: "price",
+      d: "special device"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "24. Today, doctors advise people to eat carefully. For example, when buying packaged snacks, you should choose <span style='text-decoration:underline'>those</span> with low fat.",
+    answers: {
+      a: "people",
+      b: "packaged snacks",
+      c: "doctors",
+      d: "low fat"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "25. Behavioral factors, such as smoking or eating junk food, increase the risk of cancer at every stage of <span style='text-decoration:underline'>its</span> development.",
+    answers: {
+      a: "cancer",
+      b: "every stage",
+      c: "smoking",
+      d: "dietary choice"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "26. Many people do not know that beans are legumes which is the technical term for the family of plants that includes lentils and soybeans, among many <span style='text-decoration:underline'>others</span>.",
+    answers: {
+      a: "people",
+      b: "legumes",
+      c: "beans",
+      d: "plants"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "27. Despite <span style='text-decoration:underline'>its</span> size and weight, the elephant can run quite fast.",
+    answers: {
+      a: "test",
+      b: "weight",
+      c: "elephant",
+      d: "fast"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "28. Sometimes it is the difficulty of the test <span style='text-decoration:underline'>which</span> prevents a student from finishing on time. At other times, it is the length of the tests.",
+    answers: {
+      a: "test",
+      b: "student",
+      c: "length",
+      d: "difficulty"
+    },
+    correctAnswer: 'd'
+  },
+  
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
